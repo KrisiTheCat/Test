bf9842eae5fdf2a55385dbba3411007cab453bab
smgC109 <krisivs@abv.bg>
2023-12-14 15:10:09 +0200

    4 referats and not 3

--- src/bg/smg/Main.java
+++ src/bg/smg/Main.java
@@ -10,13 +10,18 @@ public class Main {
         Referat referat3 = (Referat) referat1.clone();
         referat3.setTheme("Schnappi das kleine Krokodil");
         referat3.getAuthor().setName("Gabriela Grigorova");
+        Referat referat4 = (Referat) referat1.clone();
+        referat4.setTheme("Liberty");
+        referat4.getAuthor().setName("Stefanie Popova");
 
         referat1.submit();
         referat2.submit();
         referat3.submit();
+        referat4.submit();
 
         System.out.println(referat1.toString());
         System.out.println(referat2.toString());
         System.out.println(referat3.toString());
+        System.out.println(referat4.toString());
     }
 }
